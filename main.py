e8a0db4e88cd7e1ae9def51a3db0b35693b252c7
Luis Pablo <luismpablo@gmail.com>
2017-07-05 08:49:42 -0400

    Rewrite into classes

--- main.py
+++ main.py
@@ -1,86 +1,183 @@
 import cv2
 import numpy as np
+import os
+import csv
+import argparse
+import json
+import pprint
+from templates import heroes, win_state, TEMPLATE_DIR
 from matplotlib import pyplot as plt
 
 
-def get_positions(input_image_src, template_image_src, debug=None, text=''):
-    img_rgb = cv2.imread(input_image_src)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-    template = cv2.imread(template_image_src, 0)
-    w, h = template.shape[::-1]
+class MatchResults(object):
+    def __init__(self, win_states, player_heroes, opponent_heroes):
+        rounds = []
+        for round_win, p_hero, o_hero in zip(win_states, player_heroes, opponent_heroes):
+            print round_win
+            rounds.append({
+                'player_win': round_win[0] == 'PLAYER_WON',
+                'player_hero': p_hero,
+                'opponent_hero': o_hero,
+                'opponent_win': round_win[0] == 'PLAYER_LOST'
+            })
+        self.data = rounds
 
-    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCORR_NORMED)
-    threshold = 0.95
-    loc = np.where(res >= threshold)
+    def to_json(self):
+        return self.data
 
-    s = set()
-    previous = None
-    for pt in zip(*loc[::-1]):
-        if previous is not None:
-            if not can_merge(previous, pt, min(w, h)):
-                if not any([can_merge(item, pt, min(w, h)) for item in s]):
-                    s.add(pt)
-        else:
-            s.add(pt)
-        previous = pt
-
-    if debug is not None:
-        for pt in s:
-            cv2.putText(img_rgb, text, pt, cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
-
-        cv2.imwrite('{}.png'.format(debug), img_rgb)
-
-    return s
+    def to_csv(self, filename):
+        headers = self.data[0].keys() if self.data else []
+        with open(filename, 'wb') as out:
+            csv_writer = csv.DictWriter(out, headers=headers)
+            csv_writer.writeheader()
+            csv_writer.writerows(self.data)
 
 
-def can_merge(pair1, pair2, threshold=5):
-    x1, y1 = pair1
-    x2, y2 = pair2
+class Template(object):
+    def __init__(self, name, template_src):
+        self.name = name
+        self.template_dir = TEMPLATE_DIR
+        self.template = cv2.imread(os.path.join(TEMPLATE_DIR, template_src), 0)
+        w, h = self.template.shape[::-1]
+        self.width = w
+        self.height = h
 
-    if x1 == x2 and y1 == y2:
-        return True
-    elif abs(x1 - x2) <= threshold and abs(y1 - y2) <= threshold:
-        return True
-    else:
-        return False
 
-PLAYER_WON = 'templates/player_won.jpg'
-PLAYER_LOST = 'templates/player_lost.jpg'
-INPUT_IMAGE = 'winscreen.jpg'
+class ScreenshotScanner(object):
+    def __init__(self, method=cv2.TM_CCORR_NORMED):
+        self.heroes = heroes
+        self.hero_templates = {}
+        self.win_state_templates = {}
 
-# scan for win and loss markers
-wins = get_positions(INPUT_IMAGE, PLAYER_WON, 'res', text='won')
-losses = get_positions('res.png', PLAYER_LOST, 'res', text='lost')
+        self.load_win_state_templates()
+        self.load_hero_templates()
+        self.method = method
 
-# combine the result markers and sort
-all_stats = wins.union(losses)
-sorted_by_position = sorted(all_stats, key=lambda s: s[0])
-rounds = [(s in wins) for s in sorted_by_position]
 
-# use the y value as the marker whether
-# a character scanned is by the player or their opponent
-midpoint = sorted_by_position[0][1]
+    def load_hero_templates(self):
+        for hero in self.heroes:
+            self.hero_templates[hero['name']] = Template(hero['name'], hero['template_src'])
 
-player_rounds = {}
-opp_rounds = {}
+    def load_win_state_templates(self):
+        for item in win_state:
+            self.win_state_templates[item['name']] = Template(item['name'], item['template_src'])
 
-# scan for every hero
-for character in ['ana', 'genji', 'hanzo', 'junkrat',
-                  'mccree', 'mei', 'pharah', 'reaper',
-                  'roadhog', 'soldier76', 'sombra', 'tracer',
-                  'widowmaker', 'zenyatta']:
-    positions = get_positions('res.png', 'templates/{}.jpg'.format(character), None)
-    if positions:
-        for p in positions:
-            if p[1] > midpoint:
-                player_rounds[p] = character
+    def merge_points(self, points, threshold):
+        merged = set()
+
+        previous = None
+        for pt in points:
+            if previous is not None:
+                if not self._can_merge(previous, pt, threshold):
+                    if not any([self._can_merge(item, pt, threshold) for item in merged]):
+                        merged.add(pt)
             else:
-                opp_rounds[p] = character
+                merged.add(pt)
+            previous = pt
 
-player_characters = [player_rounds[c] for c in sorted(player_rounds, key=lambda pr: pr[0])]
-opp_characters = [opp_rounds[c] for c in sorted(opp_rounds, key=lambda pr: pr[0])]
+        return merged
 
-print "Round,Luis Hero,Jaime Hero,Winner"
-for index, result in enumerate(zip(rounds, player_characters, opp_characters)):
-    did_win, player, opponent = result
-    print ','.join([str(index + 1), player.title(), opponent.title(), 'Luis' if did_win else 'Jaime'])
+    def _can_merge(self, pair1, pair2, threshold=5):
+        x1, y1 = pair1
+        x2, y2 = pair2
+
+        return (x1 == x2 and y1 == y2) or (abs(x1 - x2) <= threshold and abs(y1 - y2) <= threshold)
+
+    def _generate_debug(self, points, debug_image, label):
+        for pt in points:
+            cv2.putText(debug_image, label, pt, cv2.FONT_HERSHEY_PLAIN, 1, (255, 255, 255))
+
+            cv2.imwrite('debug.png', debug_image)
+
+    def _extract_data(self, input_data, template_group, confidence_threshold=0.95, with_debug=None):
+        extracted = {}
+        for title, data in template_group.iteritems():
+            res = cv2.matchTemplate(input_data, data.template, self.method)
+            loc = np.where(res >= confidence_threshold)
+
+            points = self.merge_points(zip(*loc[::-1]), min(data.width, data.height))
+            extracted[title] = points
+
+            if with_debug is not None:
+                self._generate_debug(points, with_debug, title)
+
+        return extracted
+
+    def _process_win_state_data_points(self, data_points):
+        combined_win_states = []
+        for key, values in data_points.iteritems():
+            combined_win_states += [(key, v) for v in values]
+
+        return sorted(combined_win_states, key=lambda d: d[1][0])
+
+    def _process_hero_data_points(self, data_points, midpoint):
+        player = {}
+        opponent = {}
+
+        for hero, dp in data_points.iteritems():
+            for point in dp:
+                x, y = point
+                if y > midpoint:
+                    player[point] = hero
+                else:
+                    opponent[point] = hero
+
+        p_points = sorted([hero for point, hero in player.iteritems()], key=lambda d: d[0])
+        o_points = sorted([hero for point, hero in opponent.iteritems()], key=lambda d: d[0])
+
+        return p_points, o_points
+
+
+    def process(self, input_src, confidence_threshold=0.95, with_debug=False):
+        img_rgb = cv2.imread(input_src)
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+        debug_image = cv2.imread(input_src)
+
+        win_state_data_points = self._extract_data(img_gray,
+                                            self.win_state_templates,
+                                            confidence_threshold,
+                                            debug_image if with_debug else None)
+
+        sorted_win_states = self._process_win_state_data_points(win_state_data_points)
+
+        if len(sorted_win_states) == 0:
+            raise RuntimeError("No win states found")
+
+        midpoint = sorted_win_states[0][1][1]
+        hero_data_points = self._extract_data(img_gray,
+                                              self.hero_templates,
+                                              confidence_threshold,
+                                              debug_image if with_debug else None)
+
+        sorted_player_points, sorted_opponent_points = self._process_hero_data_points(hero_data_points, midpoint)
+
+        return MatchResults(sorted_win_states, sorted_player_points, sorted_opponent_points)
+
+
+def main():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('input', help='The input file to process')
+    argparser.add_argument('--debug', action='store_true', help='Produce a debug image output')
+    argparser.add_argument('--confidence', type=float, default=0.95)
+    argparser.add_argument('--output', help='Optional output filename')
+    argparser.add_argument('--output-type', choices=['json', 'csv'])
+
+    args = argparser.parse_args()
+
+    ss = ScreenshotScanner()
+    results = ss.process(args.input, confidence_threshold=args.confidence, with_debug=args.debug)
+
+    if args.output:
+        if args.output_type == 'json':
+            with open(args.output, 'w') as outfile:
+                outfile.write(json.dumps(results.to_json()))
+        elif args.output_type == 'csv':
+            results.to_csv(args.output)
+    else:
+        pprint.pprint(json.dumps(results.to_json()))
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ templates.py
@@ -0,0 +1,53 @@
+TEMPLATE_DIR = 'templates'
+
+heroes = [{
+    'name': 'Ana',
+    'template_src': 'ana.jpg'
+}, {
+    'name': 'Genji',
+    'template_src': 'genji.jpg'
+}, {
+    'name': 'Hanzo',
+    'template_src': 'hanzo.jpg'
+}, {
+    'name': 'Junkrat',
+    'template_src': 'junkrat.jpg'
+}, {
+    'name': 'McCree',
+    'template_src': 'mccree.jpg'
+},{
+    'name': 'Mei',
+    'template_src': 'mei.jpg'
+}, {
+    'name': 'Pharah',
+    'template_src': 'pharah.jpg'
+}, {
+    'name': 'Reaper',
+    'template_src': 'reaper.jpg'
+}, {
+    'name': 'Roadhog',
+    'template_src': 'roadhog.jpg'
+}, {
+    'name': 'Soldier76',
+    'template_src': 'soldier76.jpg'
+}, {
+    'name': 'Sombra',
+    'template_src': 'sombra.jpg'
+}, {
+    'name': 'Tracer',
+    'template_src': 'tracer.jpg'
+}, {
+    'name': 'Widowmaker',
+    'template_src': 'widowmaker.jpg'
+}, {
+    'name': 'Zenyatta',
+    'template_src': 'zenyatta.jpg'
+}]
+
+win_state = [{
+    'name': 'PLAYER_WON',
+    'template_src': 'player_won.jpg'
+}, {
+    'name': 'PLAYER_LOST',
+    'template_src': 'player_lost.jpg'
+}]
